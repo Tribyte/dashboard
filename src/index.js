aa0974a78c275dfa5826f13baa21d87dc9cbfc62
herbglitch <herbglitch@protonmail.com>
2022-03-11 16:30:05 -0700

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './pages/App';
+
+import { AllThemes } from './themes/AllThemes';
+import { setLocal } from './utils/storage';
+
+const Index = () => {
+    const allThemes = AllThemes();
+    setLocal('all-themes', allThemes);
+    return <App />;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Index />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
